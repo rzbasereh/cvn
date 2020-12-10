a2bcc85a91099bac7ad4a03308dfee43a675f4dc
RezaBasereh <rzbasereh1@gmail.com>
2020-12-10 11:41:20 +0330

    create Home component and install react bootstrap

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return super.render();
+    }
+}
+
+export default Home;
